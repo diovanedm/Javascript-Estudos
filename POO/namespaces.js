bee86b6c32297fe9419abd8da505a7452c882c7a
diovanedm <diovane.dm@gmail.com>
2020-06-26 18:59:57 -0300

    namespaces

---
+++ POO/namespaces.js
@@ -0,0 +1,32 @@
+// Namespaces global 
+var MEUAPP = {};
+
+MEUAPP.metodosEmComum = {
+    regexParaNome: "", 
+    regexParaTelefone: ""
+}
+
+MEUAPP.eventos = {
+    adicionarTratador: function( elemento, tipo, funcao ) {
+        // Códigos
+    },
+
+    removerTratador: function( elemento, tipo, funcao ) {
+        // Códigos
+    },
+
+    obterEventos: function( e ) {
+        // Códigos
+    }
+
+    // É possivel adicionar outros métodos e propriedades
+}
+
+MEUAPP.soma = {
+    calcularSoma: function( num1, num2 ){
+        return num1 + num2;
+    }
+}
+
+
+
